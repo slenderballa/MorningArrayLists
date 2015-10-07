a35ed0f184541830e12b6704aec97c0b3eabeb53
slenderballa <gswe4645@740s-pl-129.local>
2015-10-07 09:56:16 -0600

    Popup
    Added pop up

--- src/lists/controller/ArrayListController.java
+++ src/lists/controller/ArrayListController.java
@@ -1,6 +1,29 @@
 package lists.controller;
 
+import java.util.ArrayList; 
+
+import javax.swing.JOptionPane;
+
 public class ArrayListController
 {
-
+	private ArrayList<String> firstWords;
+	
+	public ArrayListController()
+	{
+		firstWords = new ArrayList<String>();
+	}
+	
+	public void start()
+	{
+		firstWords.add("a;lkfjaslfkjasldkfjaslkj");
+		firstWords.add("babababababababa");
+	}
+	
+	private void showList()
+	{
+		for(int spot = 0; spot < firstWords.size(); spot ++)
+		{
+			JOptionPane.showMessageDialog(null, firstWords.get(spot) = " is at spot#" + spot);
+		}
+	}
 }
